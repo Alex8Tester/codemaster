09581316b9fc2f33928fc55571c86a0ff63f3263
Alex B <alex.8ulavin@gmail.com>
2023-09-09 00:04:40 +0300

    Example_005 Hello Username IfElse

--- Example_005/Program.cs
+++ Example_005/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Hello Username
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Настя")
+{
+    Console.WriteLine("Ура, это же НАСТЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
