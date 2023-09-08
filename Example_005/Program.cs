e120ecd62a6d88ef8af25411a928ba8c84986e7b
Alex B <alex.8ulavin@gmail.com>
2023-09-09 00:24:00 +0300

    add Example_005 Push to HIT

---
+++ Example_005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "nastya")
+{
+    Console.WriteLine("Ура, это же NASTYA!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
