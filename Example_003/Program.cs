d2ecaf386ee04be1b24faebcda99a09fe99227b7
Alex B <alex.8ulavin@gmail.com>
2023-09-08 23:50:33 +0300

    Example003 add NewRandom

--- Example_003/Program.cs
+++ Example_003/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Сложение двух целых чисел в C#
 
-int numberA = 31;
-int numberB = 7;
+int numberA = new Random().Next(1, 10); // min 1 max 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
