9d03156573d81a22dabcd1217c1d56501d00e5ed
Alex B <alex.8ulavin@gmail.com>
2023-09-11 23:01:10 +0300

    Example_009 Квадрат числа

---
+++ Example_009/Program.cs
@@ -0,0 +1,10 @@
+﻿// Квадрат числа (число умноженное на само себя)
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number=int.Parse(Console.ReadLine());
+int sqr=number*number;
+Console.WriteLine($"Квадрат числа = {sqr}");
+
+
