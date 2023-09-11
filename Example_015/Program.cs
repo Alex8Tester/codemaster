32d8befe6b6750a85b919017910f357bf4d05af0
Alex B <alex.8ulavin@gmail.com>
2023-09-12 01:11:39 +0300

    Example_015 Сравнение чисел

---
+++ Example_015/Program.cs
@@ -0,0 +1,17 @@
+﻿// Сравнение трех чисел
+
+using System;
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+    if (A > B)
+    {
+        Console.Write("Число A больше");
+    }
+    else
+    {
+        Console.Write("Число B больше");
+    }
