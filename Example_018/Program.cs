fb9a508f6dddf2e5b092514262c1b127d6eba427
Alex B <alex.8ulavin@gmail.com>
2023-09-12 01:57:46 +0300

    Example_018 Четные числа от 1 до N

---
+++ Example_018/Program.cs
@@ -0,0 +1,19 @@
+﻿// Все четные числа от 1 до N
+using System;
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int B = 1;
+
+while(B <= N) 
+
+if (B % 2 == 0)
+{
+    Console.Write($"{B} ");
+    B++;
+}
+else
+{
+    B++;
+}
