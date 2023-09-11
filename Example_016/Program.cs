0d343522b3e2427c23abd8ca433bbd18d4e9e74f
Alex B <alex.8ulavin@gmail.com>
2023-09-12 01:19:35 +0300

    Example_016 Сравнение трёх чисел

---
+++ Example_016/Program.cs
@@ -0,0 +1,23 @@
+﻿// Сравнение трёх чисел
+
+using System;
+Console.Clear();
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+    if (A > B)
+    {
+        Console.Write($"Число A больше и оно равняется: {A}");
+    }
+    else if (B > C)
+    {
+        Console.Write($"Число B больше и оно равняется: {B}");
+    }
+    else 
+    {
+        Console.Write($"Число C больше и оно равняется: {C}");
+    }
