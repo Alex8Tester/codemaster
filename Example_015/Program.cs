b09d766517af1aa20d302f1e4e952eec13b98d90
Alex B <alex.8ulavin@gmail.com>
2023-09-12 02:20:38 +0300

    Example_015 fix

--- Example_015/Program.cs
+++ Example_015/Program.cs
@@ -7,11 +7,15 @@ int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
 
-    if (A > B)
-    {
-        Console.Write("Число A больше");
-    }
-    else
-    {
-        Console.Write("Число B больше");
-    }
+if (A > B)
+{
+    Console.Write($"Число A больше и оно равняется {A}, число B меньше и оно равняется {B}");
+}
+else if (A < B)
+{
+    Console.Write($"Число B больше и оно равняется {B}, число A меньше и оно равняется {A}");
+}
+else if (A == B)
+{
+    Console.Write($"Число А {A} и Число B {B} равны");
+}
