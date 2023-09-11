e41430c452e39a53124ea9a6b94288692fab4329
Alex B <alex.8ulavin@gmail.com>
2023-09-12 01:12:19 +0300

    Example_015 Сравнение чисел commitment

--- Example_011/Program.cs
+++ Example_011/Program.cs
@@ -4,7 +4,7 @@ Console.Clear();
 
 Console.Write("Введите число A: ");
 int A=int.Parse(Console.ReadLine());
-Console.Write("Введите число b: ");
+Console.Write("Введите число B: ");
 int B=int.Parse(Console.ReadLine());
     
     if (B * B == A)
