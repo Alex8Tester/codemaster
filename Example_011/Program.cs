4af62e5343624e101a80620d6785927ffee43db0
Alex B <alex.8ulavin@gmail.com>
2023-09-11 23:37:11 +0300

    Example_011 B * B == A

---
+++ Example_011/Program.cs
@@ -0,0 +1,17 @@
+﻿// Проверить является ли первое число, квадратом второго
+
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int A=int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int B=int.Parse(Console.ReadLine());
+    
+    if (B * B == A)
+    {
+    Console.WriteLine("yes");
+    }
+    else
+    { 
+    Console.WriteLine("no");
+    }
