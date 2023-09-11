21b655bc6d295b97518465c8ac4858a7545dd3b4
Alex B <alex.8ulavin@gmail.com>
2023-09-12 02:39:30 +0300

    Example_017 Fix

--- Example_017/Program.cs
+++ Example_017/Program.cs
@@ -7,9 +7,9 @@ int N = Convert.ToInt32(Console.ReadLine());
     
 if (N % 2 == 0)
     {
-        Console.Write("Ваше число - четное и оно делится на 2");
+        Console.Write("Ваше число - четное и оно делится без остатка на 2");
     }
 else
     {
-        Console.Write("Число нечетное - оно не делится на 2");
+        Console.Write("Число нечетное - оно не делится без остатка на 2");
     }    
