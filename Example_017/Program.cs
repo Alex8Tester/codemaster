cf28a84563ad3a55067f86446cdb8d855dd1827d
Alex B <alex.8ulavin@gmail.com>
2023-09-12 01:35:48 +0300

    Example_17 Четное/Нечетное

---
+++ Example_017/Program.cs
@@ -0,0 +1,15 @@
+﻿// Четное или нечетное
+
+using System;
+Console.Clear();
+Console.Write("Введите число для проверки: ");
+int N = Convert.ToInt32(Console.ReadLine());
+    
+if (N % 2 == 0)
+    {
+        Console.Write("Ваше число - четное и оно делится на 2");
+    }
+else
+    {
+        Console.Write("Число нечетное - оно не делится на 2");
+    }    
