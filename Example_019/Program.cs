8d51e128f92eb982effba33da2b33220717964db
Alex B <alex.8ulavin@gmail.com>
2023-09-12 23:26:31 +0300

    Example_019 Elementary Method

---
+++ Example_019/Program.cs
@@ -0,0 +1,24 @@
+﻿// Methods and functions
+using System;
+Console.Clear();
+int a1 = 11;
+int b1 = 21;
+int c1 = 31;
+int a2 = 12;
+int b2 = 22;
+int c2 = 322;
+int a3 = 13;
+int b3 = 2323;
+int c3 = 33;
+
+int max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
