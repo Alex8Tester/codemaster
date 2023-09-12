bcc70981bc7b3084ba7aab835a1e7a852739778e
Alex B <alex.8ulavin@gmail.com>
2023-09-13 01:04:38 +0300

    Example_022 Massive Constant method

---
+++ Example_022/Program.cs
@@ -0,0 +1,21 @@
+﻿// Massive Example 2 Use Constant 
+
+using System;
+Console.Clear();
+
+int[] array = {10, 21, 32, 87, 43, 54, 4, 76, 87};
+int n = array.Length;
+int find = 87;
+int index = 0;
+
+while (index < n)
+{
+    
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
