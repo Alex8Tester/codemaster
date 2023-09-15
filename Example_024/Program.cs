3dfbf00c230e267f3512d40dcc3916857e587a5d
Alex B <alex.8ulavin@gmail.com>
2023-09-15 22:48:39 +0300

    Example_024 Highest Number on Random Line

---
+++ Example_024/Program.cs
@@ -0,0 +1,18 @@
+﻿// Наибольшее числа из отрезка    
+using System;
+
+using static System.Console;
+
+Clear();
+int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number % 10;
+
+if (a1 > a2)
+{
+    WriteLine($"{number}->{a1}");
+}
+else
+{
+    WriteLine($"{number}->{a2}");
+}
