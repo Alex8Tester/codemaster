ddcbb33a69b140d93ea6bce2baaf6f79f87e3e08
Alex B <alex.8ulavin@gmail.com>
2023-09-15 23:20:49 +0300

    Example_025 ThreeDigestNumber delete Two digest

---
+++ Example_025/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывод трехзначного числа и удаление второй цифры из этого числа   
+using System;
+using static System.Console;
+Clear();
+
+int ThreeDigestNumber = new Random().Next(100, 1000);
+int a1 = ThreeDigestNumber / 100;
+int a2 = ThreeDigestNumber % 10;
+
+WriteLine($"Это трехзначное число: {ThreeDigestNumber} Удаляем вторую цифру и получаем: {a1*10+a2}");
+
