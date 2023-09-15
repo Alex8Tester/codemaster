9f2d7cfab629f22b6d4b4483b707aaa3417f0d2f
Alex B <alex.8ulavin@gmail.com>
2023-09-16 00:06:37 +0300

    Example_027 Number A % Number Bв

---
+++ Example_027/Program.cs
@@ -0,0 +1,19 @@
+﻿// Число А кратно Б  
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите число A: ");
+double A = Convert.ToInt32(Console.ReadLine());
+Write("Введите число B: ");
+double B = Convert.ToInt32(Console.ReadLine());
+double C = A % B;
+
+if (A % B == 0)
+{
+    Write($"Число {B} кратно числу {A}");
+}
+else
+{
+    Write($"Число {B} некратно числу {A} где остаток от деления {C}");
+}
