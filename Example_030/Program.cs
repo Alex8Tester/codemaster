acd789556c6fd4f20c94ddafdfa686addb8cbc84
Alex B <alex.8ulavin@gmail.com>
2023-09-17 00:04:46 +0300

    Example_030 ThreeDigestNumber and second rank

---
+++ Example_030/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вывод второй цифры (разряда) в трёхзначном числе
+using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите трёхзначное число: ");
+int ThreeDigestNumber = int.Parse(ReadLine());
+
+if (ThreeDigestNumber > 100 && ThreeDigestNumber < 1000)
+{
+    WriteLine($"Введенное число {ThreeDigestNumber} где второе число равно {ThreeDigestNumber / 10 % 10}");
+}
+else
+{
+    WriteLine ("Вы ввели число не соответствующее трехзначному, попробуйте заново");
+}
+
+
