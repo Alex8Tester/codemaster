7870cdfc90e4a09e7dbdbd9164c53ecdd143849f
Alex B <alex.8ulavin@gmail.com>
2023-09-16 23:42:54 +0300

    Example_029 A round B and reverse

---
+++ Example_029/Program.cs
@@ -0,0 +1,25 @@
+﻿// Два числа (Число А квадрат Б) или наоборот
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите число A: ");
+int A=int.Parse(ReadLine());
+Write("Введите число B: ");
+int B=int.Parse(ReadLine());
+    
+    if (B * B == A)
+    {
+        WriteLine($"Да, число B = {B} является квадратом числа A = {A}");
+    }
+    else
+    {   
+        if (Math.Pow(A, 2) == B)
+        {
+            WriteLine($"Да A = {A} является квадратом числа B = {B}");        
+        }
+        else
+        {
+            WriteLine("Число А не является квадратом числа B, как и ровно наоборот");
+        }
+    }
