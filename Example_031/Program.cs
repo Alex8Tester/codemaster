7b64041d7c93709ba1904381df55bfa411bb4881
Alex B <alex.8ulavin@gmail.com>
2023-09-17 02:09:30 +0300

    Example_031 Digest -- Three Rank on this digest

---
+++ Example_031/Program.cs
@@ -0,0 +1,41 @@
+﻿// Вывод третьей цифры (разряда) в заданном числе
+using System;
+using static System.Console;
+Clear();
+
+int ParseLine(string text) 
+{
+    WriteLine(text);
+    string value = ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int ThreeDigestRank(int digest)
+{
+    while (digest > 999)
+    {
+        digest /= 10;
+    }
+    return digest % 10;
+}
+
+bool ValidateDigest(int digest)
+{
+    if (digest < 100)
+    {
+        WriteLine("В данном примере нет третьей цифры (разряда)");
+        return false;
+    }
+    return true;
+}
+
+int digest = ParseLine("Введите числовое значение: ");
+if (ValidateDigest(digest))
+{
+    WriteLine(ThreeDigestRank(digest));
+}
+
+
+
+
