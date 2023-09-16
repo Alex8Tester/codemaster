b8bbf7bc2f7d8c9238060b4894ecf64e182ca564
Alex B <alex.8ulavin@gmail.com>
2023-09-17 02:48:47 +0300

    Example_032 DayOfTheWeek v2.0

---
+++ Example_032/Program.cs
@@ -0,0 +1,23 @@
+﻿// Дни недели - проверить будний день или выходной
+using System;
+using static System.Console;
+Clear();
+
+Console.Write("Введите число дня недели от 1 до 7: ");
+int number = Convert.ToInt32(ReadLine());
+
+void CheckTheDay(int number) {
+    if (number > 0 && number < 5)
+        {
+            WriteLine($"Это {number} и это будний день");
+        }
+    else if (number == 6 || number == 7)
+        {
+            WriteLine($"Это {number} и это выходной день");
+        }
+    else if (number == 0 || number > 7)
+        {
+            WriteLine("А это вообще не день недели, попробуй снова");
+        }
+}
+CheckTheDay(number);
