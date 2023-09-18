0698e8c70ade1901fb9cd25210b48c0f8ce0b0ea
Alex B <alex.8ulavin@gmail.com>
2023-09-18 21:24:37 +0300

    Example_033 Coordinates X and Y, and position on system coordinates

---
+++ Example_033/Program.cs
@@ -0,0 +1,26 @@
+﻿// Координаты X и Y, где X != 0 и Y != 0
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите координату X: ");
+int x=int.Parse(ReadLine());
+Write("Введите координату Y: ");
+int y=int.Parse(ReadLine());
+
+if(x > 0 && y > 0)
+{
+    WriteLine("1 четверть");
+}
+if(x < 0 && y > 0)
+{
+    WriteLine("2 четверть");
+}
+if(x < 0 && y < 0)
+{
+    WriteLine("3 четверть");
+}
+if(x > 0 && y < 0)
+{
+    WriteLine("4 четверть");
+}
