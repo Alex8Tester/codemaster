9a36f173b6c1c3ce05187df0233afdcd9a9b1c08
Alex B <alex.8ulavin@gmail.com>
2023-09-18 22:52:26 +0300

    Example_035 Theorem of Pifagor

---
+++ Example_035/Program.cs
@@ -0,0 +1,17 @@
+﻿// Координаты X и Y, Теорема Пифагора
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите координату X1: ");
+int X1=int.Parse(ReadLine());
+Write("Введите координату Y1: ");
+int Y1=int.Parse(ReadLine());
+Write("Введите координату X2: ");
+int X2=int.Parse(ReadLine());
+Write("Введите координату Y2: ");
+int Y2=int.Parse(ReadLine());
+
+double D = Math.Sqrt(Math.Pow(X1-X2,2)+Math.Pow(Y1-Y2,2));
+WriteLine($"Длина отрезка = {D:f2}");
+
