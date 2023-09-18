1b2eaa6129324039521cf17bae3d86c45722d3f4
Alex B <alex.8ulavin@gmail.com>
2023-09-18 23:17:01 +0300

    Example_036 Digest 1 do N on Sqr(N)

---
+++ Example_036/Program.cs
@@ -0,0 +1,12 @@
+﻿// Числа от 1 до N, вывести квадрат числа N
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+for(int i=1;i<=N;i++)
+{
+    Write($" {Math.Pow(i,2)} ");
+}
