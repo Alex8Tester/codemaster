7f8763c6d13c1ef5ddb2a6217050511ccd552bf1
Alex B <alex.8ulavin@gmail.com>
2023-09-20 23:14:12 +0300

    Example_040 Examples on tasks

---
+++ Example_040/Program.cs
@@ -0,0 +1,44 @@
+﻿// Методы в C# Примеры Задач
+
+using System;
+using static System.Console;
+Clear();
+
+// ====== Работа с текстом ======
+//Дан текст. В тексте нужно все пробелы заменить черточками,
+//маленькие буквы "к" заменить большими "К",
+//а больше "С" заменить маленькими "с"
+
+string text = "- Я думаю, - Сказал князь, улыбаясь, - что, "
+            + "ежели бы ваС поСлали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие пруССкого короля. "
+            + "Вы так краСноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+WriteLine(newText);
+WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+WriteLine(newText);
+WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+WriteLine(newText);
+WriteLine();
