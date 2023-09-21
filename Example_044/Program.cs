8140fe09e6620db88d02348057c5a678e6d8b6ea
Alex B <alex.8ulavin@gmail.com>
2023-09-21 20:56:17 +0300

    Example_044 Factorial 1 do N

---
+++ Example_044/Program.cs
@@ -0,0 +1,20 @@
+﻿// Example_044 Числа от 1 до N произведение чисел
+using System;
+using static System.Console;
+Clear();
+// Method use 
+
+WriteLine("Введите число N: ");
+int N=int.Parse(ReadLine());
+int sum=SummaryNum(N);
+WriteLine($"Сумма = {sum}");
+
+int SummaryNum(int number)
+{
+    int num=1;
+    for(int i=1;i<=number;i++)
+    {
+       num = num * i; 
+    }
+    return num;
+}
