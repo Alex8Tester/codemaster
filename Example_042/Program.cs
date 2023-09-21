07875f63bdf5159ff6c263e89215360658e11cfc
Alex B <alex.8ulavin@gmail.com>
2023-09-21 20:13:28 +0300

    Example_042 Sum 1 do A

---
+++ Example_042/Program.cs
@@ -0,0 +1,21 @@
+﻿// Example_042 Number 1 do A
+using System;
+using static System.Console;
+Clear();
+
+// Using method
+
+WriteLine("Введите число A: ");
+int A=int.Parse(ReadLine());
+int summa=GetSumNumbers(A);
+WriteLine($"Сумма = {summa}");
+
+int GetSumNumbers(int number)
+{
+    int sum=0;
+    for(int i=1;i<=number;i++)
+    {
+        sum+=i;
+    }
+    return sum;
+}
