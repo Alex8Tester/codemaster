3480a8b80eafc94e78bc314b541f7a6023a6f1fc
Alex B <alex.8ulavin@gmail.com>
2023-09-21 20:27:51 +0300

    Example_043 GetDigestOnNumber

---
+++ Example_043/Program.cs
@@ -0,0 +1,21 @@
+﻿// Example_043 Количество цифр в числе
+using System;
+using static System.Console;
+Clear();
+// Method use
+
+WriteLine("Введите число для подсчета цифр: ");
+int A=int.Parse(ReadLine());
+int nums=GetDigestOnNumber(A);
+WriteLine($"Сумма = {nums}");
+
+int GetDigestOnNumber(int number)
+{
+    int count=0;
+    while(number>0)
+    {
+        count++;
+        number/=10;
+    }
+    return count;
+}
