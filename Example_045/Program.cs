d83244239500d588893ed431b5b729a49785252c
Alex B <alex.8ulavin@gmail.com>
2023-09-21 21:17:06 +0300

    Example_045 RandomBinaryArray

---
+++ Example_045/Program.cs
@@ -0,0 +1,17 @@
+﻿// Example_045 Array Binary Random
+using System;
+using static System.Console;
+Clear();
+// Method use 
+WriteLine($"[{string.Join(",",RandomBinaryArray(8))}]");
+
+
+int[] RandomBinaryArray(int size)
+{
+    int [] array = new int[size];
+    for(int i=0;i<size;i++)
+    {
+        array[i]=new Random().Next(0, 2);
+    }
+    return array;
+}
