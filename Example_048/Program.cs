b43813a72663fc71961977e14b9a5bdb0b433102
Alex B <alex.8ulavin@gmail.com>
2023-09-23 23:46:10 +0300

    Example_048 New Random Array

---
+++ Example_048/Program.cs
@@ -0,0 +1,42 @@
+﻿// Example_047 принимаем на вход число и возвращаем сумму цифр в нем
+using System;
+using static System.Console;
+Clear();
+// Method use 
+
+int size = Vvod("Введите длину массива");
+int minimalDigest = Vvod("Введите минимальный элемент массива");
+int maximalDigest = Vvod("Введите максимальный элемент массива");
+int [] array = RandomArray(size, minimalDigest, maximalDigest);
+PrintArray(array);
+
+int Vvod(string message)
+{
+    WriteLine(message);
+    string readMessage = ReadLine();
+    int result = int.Parse(readMessage);
+    return result; 
+}
+
+int[] RandomArray(int length, int min, int max)
+{
+    int [] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i]= new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+       Write($"{array[i]}, "); 
+    }
+    Write($"{array[array.Length - 1]}");
+    WriteLine("]");
+}
+
+
