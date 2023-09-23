8a91ffabc2e98a7905a89431dfbe4428b34dff98
Alex B <alex.8ulavin@gmail.com>
2023-09-24 02:49:21 +0300

    Example_054 Find elements in array, where is [10:99]

---
+++ Example_054/Program.cs
@@ -0,0 +1,34 @@
+﻿// Example_054
+using System;
+using static System.Console;
+Clear();
+// Method use 
+int[] array = GetArray(10, 0, 1000);
+WriteLine(String.Join(",",array));
+int countElements = GetCountElements(array, 10, 99);
+WriteLine($"Количество элементов в отрезке [10:99] = {countElements}");
+
+int GetCountElements(int[] inArray, int leftRange, int rightRange)
+{
+    int count = 0;
+    foreach(int item in inArray)
+    {
+        if(item >= leftRange && item <= rightRange)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
