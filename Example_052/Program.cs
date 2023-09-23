bb56fb6d04047f98fb8d36f5432f189c997830b6
Alex B <alex.8ulavin@gmail.com>
2023-09-24 02:22:21 +0300

    Example_052 Find Element in array

---
+++ Example_052/Program.cs
@@ -0,0 +1,59 @@
+﻿// Example_052 
+using System;
+using static System.Console;
+Clear();
+// Method use 
+
+int size = Vvod("Введите длину массива");
+int minimalDigest = Vvod("Введите минимальный элемент массива");
+int maximalDigest = Vvod("Введите максимальный элемент массива");
+int DigestN = Vvod("Введите искомое число");
+int [] array = RandomArray(size, minimalDigest, maximalDigest);
+PrintArray(array);
+
+if(FindElement(array,DigestN))
+{
+    WriteLine($"Указанный элемент {DigestN} найден в массиве");
+}
+else
+{
+    WriteLine($"Элемент {DigestN} не найден в массиве");
+}    
+
+int Vvod(string message)
+{
+    WriteLine(message);
+    string readMessage = ReadLine();
+    int result = int.Parse(readMessage);
+    return result; 
+}
+
+int[] RandomArray(int size, int min, int max)
+{
+    int [] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i]= new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+bool FindElement(int[] array, int DigestN)
+{
+    foreach(int item in array)
+    {
+        if(item==DigestN) return true;
+    }
+    return false;
+}
+
+void PrintArray(int [] array)
+{
+    Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+       Write($"{array[i]}, "); 
+    }
+    Write($"{array[array.Length - 1]}");
+    WriteLine("]");
+}
