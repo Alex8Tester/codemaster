86bef9ac818213813d1f8a359354de1a362bb3b4
Alex B <alex.8ulavin@gmail.com>
2023-09-23 22:59:30 +0300

    Example_047 Sum of Digest

---
+++ Example_047/Program.cs
@@ -0,0 +1,27 @@
+﻿// Example_047 принимаем на вход число и возвращаем сумму цифр в нем
+using System;
+using static System.Console;
+Clear();
+// Method use 
+int sum = Vvod("Введите вашу цифру");
+WriteLine($"Сумма цифр в числе {sum} = {sumDigit(sum)}");
+
+
+int Vvod(string message)
+{
+    WriteLine(message);
+    string readMessage = ReadLine();
+    int result = int.Parse(readMessage);
+    return result; 
+}
+
+int sumDigit(int sum)
+{
+    int result = 0;
+    while (sum > 0)
+    {
+        result += sum % 10;
+        sum = sum / 10;
+    }
+    return result;
+}
