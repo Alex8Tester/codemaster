4bae182e7098de0074b5e1ea94bfff8f6097da74
Alex B <alex.8ulavin@gmail.com>
2023-09-25 01:28:56 +0300

    Example_061 Factorial

---
+++ Example_061/Program.cs
@@ -0,0 +1,17 @@
+﻿// Example_061 TwoDimensional array - Factorial
+using System;
+using static System.Console;
+Clear();
+// Use methods
+WriteLine();
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    WriteLine($"!{i} = {Factorial(i)}");
+}
