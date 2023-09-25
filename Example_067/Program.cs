71bdba234df0b92173baae0ec1d20bca34ffe695
Alex B <alex.8ulavin@gmail.com>
2023-09-26 00:17:57 +0300

    Example_067 Copy Massive

---
+++ Example_067/Program.cs
@@ -0,0 +1,37 @@
+﻿// Example_067 Array copied method (Elementary method)
+using System;
+using static System.Console;
+Clear();
+// Use methods
+WriteLine("Введите массив через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+
+int[] arrayCopy = CopyArray(array);
+WriteLine(String.Join(" ",arrayCopy));
+
+int[] array2 = array;
+WriteLine(String.Join(" ",array2));
+
+array[2] = 100;
+WriteLine(String.Join(" ",array2));
+
+int[] CopyArray(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = inArray[i];
+    }
+    return result;
+}
+
+int[] GetArrayFromString(string arrayString)
+{
+    string[] nums = arrayString.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[nums.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+       result[i] = Convert.ToInt32(nums[i]);
+    }
+    return result;
+}
