e3efe33d97a3613baac56130483309c003c3f7cf
Alex B <alex.8ulavin@gmail.com>
2023-09-25 23:41:15 +0300

    Example_066 Fibonacci Digests (Nonrecursive method)

---
+++ Example_066/Program.cs
@@ -0,0 +1,22 @@
+﻿// Example_066 Fibonacci Digests (No recursive method)
+using System;
+using static System.Console;
+Clear();
+// Use methods
+
+Write("Введите переменную N:");
+int N = int.Parse(ReadLine());
+PrintNumbers(0,1,N);
+
+void PrintNumbers(int a1, int a2, int size)
+{
+    Write($"{a1} {a2} ");
+    for(int i = 0; i < size - 2; i++)
+    {
+        Write($"{a1 + a2} ");
+        int k =  a1 + a2;
+        a1 = a2;
+        a2 = k;
+    }
+}
+
