110d54cd12e3185d14736018b786f3968097b9c9
Alex B <alex.8ulavin@gmail.com>
2023-09-25 23:13:17 +0300

    Example_064 Triangle Sides

---
+++ Example_064/Program.cs
@@ -0,0 +1,20 @@
+﻿// Example_064 Triangle sides
+using System;
+using static System.Console;
+Clear();
+// Use Methods
+WriteLine("Введите стороны треугольника через пробел");
+string[] nums=ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+if(IsTriangle(int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2])))
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
+
+bool IsTriangle(int a, int b, int c)
+{
+    return (((a + b) > c) && ((b + c) > a) && ((a + c) > b));
+}
