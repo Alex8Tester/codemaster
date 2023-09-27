bda0d6b97dfba6576f70f1d08a234e0c8ff68364
Alex B <alex.8ulavin@gmail.com>
2023-09-28 00:15:56 +0300

    Example_070 TwoDimension Array

---
+++ Example_070/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Example_70 Задайте двумерный массив размером M x N, 
+заполненный случайными целыми числами */
+using System;
+using static System.Console;
+Clear();
+// Code
+
+WriteLine("Введите размер массива через пробел: ");
+string[] nums = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+int[,] array = GetArray(int.Parse(nums[0]), int.Parse(nums[1]), -10, 10);
+PrintArray(array);
+
+// Methods
+
+int[,] GetArray(int rows, int columns, int min, int max)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
