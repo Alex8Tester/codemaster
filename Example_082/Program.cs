d3cc38c4fd488cd401edb053376b276ed7c78ead
Alex B <alex.8ulavin@gmail.com>
2023-10-03 23:47:45 +0300

    Example_082 TwoDimension array - Sorting

---
+++ Example_082/Program.cs
@@ -0,0 +1,74 @@
+﻿/* Задайте двумерный массив. 
+Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+*/
+
+using System;
+using System.Linq;
+using static System.Console;
+Clear();
+
+// Code
+
+int m = InputText("Введите количество строк: ");
+int n = InputText("Введите количество столбцов: ");
+int range = InputText("Ввеедите диапазон от 1 до ");
+
+int[,] array = new int [m, n];
+GetArray(array);
+PrintArray(array);
+
+WriteLine($"\n Отсортированный массив: ");
+OrderLines(array);
+PrintArray(array);
+
+// Methods 
+
+int InputText(string message)
+{
+    Write(message);
+    string readMessage = ReadLine();
+    int result = Convert.ToInt32(readMessage);
+    return result;
+}
+
+void GetArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(range);
+    }
+  }
+}
+
+void OrderLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write(inArray[i,j] + " ");
+        }
+        WriteLine();
+    }
+}
