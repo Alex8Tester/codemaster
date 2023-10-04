c6591174e57a5f7e3fd1a243bc105b65b6d06ad5
Alex B <alex.8ulavin@gmail.com>
2023-10-05 01:38:06 +0300

    Example_090 MathPow A(B) Recursive method

---
+++ Example_091/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Example_090 Напишите программу,
+которая будет принимать на вход число 
+и возвращать сумму его цифр */
+using System;
+using System.Linq;
+using static System.Console;
+Clear();
+
+// Code
+
+int A = InputText("Введите число: ");
+int B = InputText("Введите показатель степени: ");
+WriteLine($"Число {A} в степени {B} = {NaturalSum(A,B)}");
+
+// Methods
+
+int InputText(string message)
+{
+    Write(message);
+    string readMessage = ReadLine();
+    int result = Convert.ToInt32(readMessage);
+    return result;
+}
+
+int NaturalSum(int naturalDigit, int exponenceDigit)
+{
+    if(exponenceDigit == 0) return 1;
+    if(exponenceDigit == 1) return naturalDigit;
+    return (naturalDigit * NaturalSum(naturalDigit,exponenceDigit - 1));
+}
