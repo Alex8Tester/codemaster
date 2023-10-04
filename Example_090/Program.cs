e0977f7a37cdec3c91b82b3a7869ef12be150e7c
Alex B <alex.8ulavin@gmail.com>
2023-10-05 01:19:08 +0300

    Example_090 ThreeDigests sum

---
+++ Example_090/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Example_090 Напишите программу,
+которая будет принимать на вход число 
+и возвращать сумму его цифр
+*/
+using System;
+using System.Linq;
+using static System.Console;
+Clear();
+
+// Code
+
+int N = InputText("Введите число N: ");
+WriteLine($"Сумма цифр числа = {PrintNumber(N)}");
+
+// Methods
+
+int InputText(string message)
+{
+    Write(message);
+    string readMessage = ReadLine();
+    int result = Convert.ToInt32(readMessage);
+    return result;
+}
+
+int PrintNumber(int sum)
+{
+    if(sum == 0) return 0;
+    return(sum % 10 + PrintNumber(sum / 10));
+}
